ed9ca659d22dfe19121e8481ed2e4eb58006caab
Jesus Arrioja <Jarrioja2210@gmail.com>
2024-02-04 11:34:20 -0300

    fix things

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
-/// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly WP_URL: string;
+  readonly SPOTIFY_CLIENT_ID: string;
+  readonly SPOTIFY_CLIENT_SECRET: string;
+  readonly SITE: string;
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- src/lib/worpdress.ts
+++ src/lib/worpdress.ts
@@ -1,3 +1,4 @@
+import type { Node } from "src/interfaces/getAllUris";
 import {
   GET_ALL_URIS,
   GET_HOMEPAGE_POSTS,
@@ -9,7 +10,7 @@ import {
 const fetchQuery = async (query: string, variable?: object) => {
   return await fetch(import.meta.env.WP_URL, {
     method: "post",
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json charset=utf-8" },
     body: JSON.stringify({
       query: query,
       variables: variable,
@@ -32,7 +33,6 @@ export async function getNodeByURI(uri: string) {
 export async function homePagePostsQuery() {
   const response = await fetchQuery(GET_HOMEPAGE_POSTS);
   const { data } = await response.json();
-
   return data;
 }
 
@@ -46,23 +46,31 @@ export async function getAllUris() {
   const response = await fetchQuery(GET_ALL_URIS);
   const { data } = await response.json();
   delete data.pages; // Eliminar paginas para poder hacer build
-  const uris: Uris = Object.values(data)
-    .reduce(function (acc: any, currentValue: any) {
-      return acc.concat(currentValue.nodes);
-    }, [])
-    .filter(
-      (node: any) =>
-        node.uri !== null && node.uri !== "/favicon.ico" && node.uri !== "/"
-    )
-    .map((node: any) => {
-      let trimmedURI = node.uri.substring(1);
-      trimmedURI = trimmedURI.substring(0, trimmedURI.length - 1);
-      return {
-        params: {
-          uri: trimmedURI,
-        },
-      };
-    });
+
+  // Juntar y aplanara Uris de la web
+  const rawUris: any = Object.values(data).reduce(function (
+    previousValue: any,
+    currentValue: any
+  ) {
+    return previousValue.concat(currentValue.nodes);
+  },
+  []);
+  // Filtrar uris descartando las nula, el favicon y el homepath
+  const filterUris = rawUris.filter(({ uri }: Node) => {
+    return uri !== null && uri !== "/favicon.ico" && uri !== "/";
+  });
+
+  // mapear para darles el formato adecuado para que lo reconozca el getStaticPath()
+  const uris: Uris[] = filterUris.map(({ uri }: Node) => {
+    // let trimmedURI = uri!.substring(1);
+    // trimmedURI = trimmedURI.substring(0, trimmedURI.length - 1);
+    console.log(uri!.substring(1));
+    return {
+      params: {
+        uri: uri!.substring(1),
+      },
+    };
+  });
 
   return uris;
 }
